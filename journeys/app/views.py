f73e0cf6675314e654ba621f175bcf5c692c3b25
Adesh <adeshslad@gmail.com>
2025-02-04 22:43:49 +0530

    fixed add location

--- journeys/app/views.py
+++ journeys/app/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Journey
+from .models import Journey, Location, Location_Photo
 from datetime import datetime
 
 # Create your views here.
@@ -24,7 +24,6 @@ def add_journey(request):
         journey.title = title
         journey.start_date = start_date
         journey.user = user
-
         journey.save()
 
         return redirect('app:journeys')
@@ -41,10 +40,27 @@ def view_journey(request, slug):
 
 def add_location(request, slug):
     if request.method == 'POST':
-        
+        name = request.POST.get("name")
+        arrival = request.POST.get("arrival")
+        departure = request.POST.get("departure")
+        journey = Journey.objects.get(slug=slug)
+
+        location = Location()
+        location.name = name
+        location.arrival = arrival
+        location.departure = departure
+        location.journey = journey
+        location.save()
+
+        photos = request.FILES.getlist("location_photos")
+        for photo in photos:
+            location_photo = Location_Photo()
+            location_photo.photo = photo
+            location_photo.location = location
+            location_photo.save()
 
         return redirect('app:view_journey', slug=slug)
 
     elif request.method == 'GET':
         locations = []
-        return render(request, "add_location.html", {'locations': locations})
+        return render(request, "add_location.html", {'locations': locations, 'slug': slug})
